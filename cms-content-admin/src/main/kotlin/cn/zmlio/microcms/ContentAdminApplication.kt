9a83d5682916b9e248955df322603387708924fd
zmlgit <zmanliang@163.com>
2017-08-01 10:33:15 +0800

    cms-admin module initial

---
+++ cms-content-admin/src/main/kotlin/cn/zmlio/microcms/ContentAdminApplication.kt
@@ -0,0 +1,12 @@
+package cn.zmlio.microcms
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ContentAdminApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ContentAdminApplication::class.java, *args)
+}
+
---
+++ cms-content-admin/src/test/kotlin/cn/zmlio/microcms/HelloTest.kt
@@ -0,0 +1,5 @@
+package cn.zmlio.microcms
+
+class HelloTest {
+
+}
